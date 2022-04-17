bcde9688497d1c18c44b7a50a9c0dd7968b75ef0
aiorgulescu <70543713+aiorgulescu@users.noreply.github.com>
2022-04-17 16:49:13 -0700

    Added Stepper Logic
    Added stepper with push button logic as well as cleaned up the code a littile

--- Part2.ino
+++ Part2.ino
@@ -1,39 +1,74 @@
 #include <LiquidCrystal.h>
+#include <Stepper.h>
+
+#define STEPS 500
 
 volatile unsigned char *myADMUX = (unsigned char *)0x7C;
 volatile unsigned char *myADCSRA = (unsigned char *)0x7A;
 volatile unsigned char *myADCH = (unsigned char *) 0x79;
 volatile unsigned char *myADCL = (unsigned char *)0x78;
 
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+//pins
+const int ButtonPin = 8;
+
+const int LCD_RS = 2;
+const int LCD_E = 3;
+const int LCD_D4 = 4;
+const int LCD_D5 = 5;
+const int LCD_D6 = 6;
+const int LCD_D7 = 7;
+
+const int STEPPER_N1 = 10;
+const int STEPPER_N2 = 11;
+const int STEPPER_N3 = 12;
+const int STEPPER_N4 = 13;
+
+//state
+int buttonState = 0;
+
+//Modules
+LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+Stepper stepper(STEPS, STEPPER_N1, STEPPER_N2, STEPPER_N3, STEPPER_N4);
+
 
 void setup() {
   
   lcd.begin(16, 2);
   lcd.print("WATER LEVEL: ");
+  stepper.setSpeed(30);
+  pinMode(ButtonPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
 
-  *myADMUX |= B00000101; // Read A5
+  *myADMUX |= B00000100; // Read A4
   *myADMUX |= B11000000; //REFS1 and REFS0 to 1 to read from internal volt reference
   *myADCSRA |= B11000000; //ADEN and ADSC set to 1 to begin conversion
 
   while (bit_is_set(*myADCSRA, *myADCSRA >> 6));
 
-  int value = *myADCL | (*myADCH << 8);
+  int value = ADCL | (ADCH << 8);
   lcd.setCursor(0, 1);
-  if (value <= 400)
+  if (value <= 700)
   {
     lcd.print("Empty           ");
   }
   else
   {
+    Serial.print("In Water\n");
     lcd.print("In Water            ");
+  }
+
+  buttonState = digitalRead(ButtonPin);
+
+  if (buttonState == HIGH)
+  {
+    stepper.step(100);
   }
   
 
-  delay(1000);
+  delay(300);
 
 }
